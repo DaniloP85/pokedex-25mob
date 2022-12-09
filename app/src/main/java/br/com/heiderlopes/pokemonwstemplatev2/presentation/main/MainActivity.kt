643890b3281258abada12c2172d0ef7c68234abc
Danilo Santos <danilopsnts@gmail.com>
2022-12-08 22:05:17 -0300

    add camera para ler o qrcode

--- app/src/main/java/br/com/heiderlopes/pokemonwstemplatev2/presentation/main/MainActivity.kt
+++ app/src/main/java/br/com/heiderlopes/pokemonwstemplatev2/presentation/main/MainActivity.kt
@@ -5,12 +5,17 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import br.com.heiderlopes.pokemonwstemplatev2.R
+import br.com.heiderlopes.pokemonwstemplatev2.databinding.ActivityMainBinding
 import br.com.heiderlopes.pokemonwstemplatev2.presentation.listpokemons.ListPokemonsActivity
+import br.com.heiderlopes.pokemonwstemplatev2.presentation.scan.ScanActivity
 
 class MainActivity : AppCompatActivity() {
+
+    private val viewBinding by lazy { ActivityMainBinding.inflate(layoutInflater) }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(viewBinding.root)
 
         val btPokemonList = findViewById<Button>(R.id.btPokemonList)
 
@@ -18,5 +23,9 @@ class MainActivity : AppCompatActivity() {
             val listPokemonsIntent = Intent(this, ListPokemonsActivity::class.java)
             startActivity(listPokemonsIntent)
         }
+
+        viewBinding.btPokedex.setOnClickListener {
+            startActivity(Intent(this, ScanActivity::class.java))
+       }
     }
 }
